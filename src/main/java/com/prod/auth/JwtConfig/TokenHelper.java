45c1322c17d9ccec4876753745269bb386fdbfd5
Aayush Pathak <aayushpathak77@gmail.com>
2025-11-30 00:13:47 +0545

    added jwt config

--- src/main/java/com/prod/auth/JwtConfig/TokenHelper.java
+++ src/main/java/com/prod/auth/JwtConfig/TokenHelper.java
@@ -1,11 +1,61 @@
-@Component
-public class JWTTokenHelper {
-private String appName;
-private String secretKey;
+package com.prod.auth.JwtConfig;
 
-       private int expiration;
+import com.prod.auth.Entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.security.KeyFactory;
+import java.util.Date;
+import java.util.Map;
+
+@Builder
+public class TokenHelper {
+
+
+    private String appName;
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            User user,
+            long expiration
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        return KeyFactory();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private String extractClaim(String token, Object getSubject) {
+        return null;
+    }
 
     public String generateToken(String userName){
         return generateToken(userName, generateExpirationDate(), getSigningKey());
+    }
+
+    private long generateExpirationDate() {
+        return jwtExpiration;
     }
 }
